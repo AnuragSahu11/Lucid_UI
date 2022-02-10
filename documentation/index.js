2385c6a4adc80d93d8def44a026cc8b37b25bef5
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-02-11 00:12:58 +0530

    [feat]Added JS for hamburger to work properly

--- documentation/index.js
+++ documentation/index.js
@@ -1,7 +1,16 @@
 const hamburger = document.getElementById("hamburger");
 const componentList = document.querySelector(".component-list");
-const component = document.querySelector(".component")
+const component = document.querySelector(".component");
 
 hamburger.addEventListener("click", () => {
- 
+  component.style.display = "none";
+  componentList.style.display = "initial";
+  console.log(component.style.display);
+});
+
+componentList.addEventListener("click", () => {
+  if (component.style.display == "none") {
+    component.style.display = "block";
+    componentList.style.display = "none";
+  }
 });
