af06062b74e427f4f70fee29ab6a52b7e07b121e
Anurag Sahu <54500608+AnuragSahu11@users.noreply.github.com>
2022-02-12 23:12:46 +0530

    [feat][refactor]Added logo to Navbar and homepage

--- documentation/index.js
+++ documentation/index.js
@@ -14,3 +14,16 @@ componentList.addEventListener("click", () => {
     componentList.style.display = "none";
   }
 });
+// alert
+
+
+
+
+// code snippet
+const avatarButton = document.getElementById("avatarButton");
+const avatarCode = document.getElementById("avatarCode");
+
+avatarButton.addEventListener("click", () => {
+  avatarCode.style.display = "initial";
+  avatarButton.style.display = "none";
+});
